97d231328fb64c910cf24382d9eb96a232831c37
Iury Luciano <iurylucianodeo@gmail.com>
2022-09-18 01:52:14 -0300

    Versão 0.0.2

---
+++ cadastroindex.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" lang="pt-br">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <title>Cadastro de Clientes</title>
+</head>
+
+<body>
+<?php
+//recuperação de dados 
+
+$nome = $_GET["username"];
+$senha = $_GET["password"];
+
+//processamento
+include_once("conexaobd.php");
+
+$sql = "INSERT INTO CLINTE (NOME,SENHA) VALUES ('$nome','$senha');";
+mysqli_query($connection,$sql) or die ("Erro ao acessar o Banco de Dados");
+
+    //saida
+
+    echo "Cliente cadastrado com sucesso!<br><br>";
+    echo "<strong>Nome do cliente:</strong> $nome<br>";
+    echo "<strong>Senha do cliente:</strong> $senha<br>";
+
+?>
+
+</body>
+
+</html>
